c7e9b6c0e64929f8367c840e001ad5e1d1149cca
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-09-25 22:17:10 +0300

    Dialog/Modal fixed (kinda)

--- src/app/todo-dialog-modal/todo-dialog-modal.component.ts
+++ src/app/todo-dialog-modal/todo-dialog-modal.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormGroup, FormControl } from '@angular/forms';
+import { TodoCrudService } from '../todo-crud.service';
+import { v4 as uuid_v4 } from 'uuid';
+import { Todo } from '../models/Todo';
 
-// Dialog-komponentin modaali-puoli
+// Dialog-modaali
 @Component({
   selector: 'app-todo-dialog-modal',
   templateUrl: './todo-dialog-modal.component.html',
   styleUrls: ['./todo-dialog-modal.component.css'],
+  providers: [TodoCrudService],
 })
 export class TodoDialogModalComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<TodoDialogModalComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<TodoDialogModalComponent>,
+    private todoService: TodoCrudService
+  ) {}
 
   todoForm = new FormGroup({
     title: new FormControl(''),
@@ -19,7 +26,22 @@ export class TodoDialogModalComponent implements OnInit {
   ngOnInit(): void {}
 
   onSubmit() {
-    console.log(this.todoForm.value);
+    let newTodo: Todo;
+
+    newTodo = {
+      id: uuid_v4(),
+      title: this.todoForm.value.title,
+      body: this.todoForm.value.body,
+      completed: false,
+    };
+
+    console.log(newTodo);
+
+    this.todoService.addTodo(newTodo).then(() => {
+      this.todoForm.value.title = ''; // clear input form value
+      this.todoForm.value.body = ''; // clear input form value
+      this.dialogRef.close();
+    });
   }
 
   onNoClick(): void {
--- src/app/todo-dialog/todo-dialog.component.ts
+++ src/app/todo-dialog/todo-dialog.component.ts
@@ -1,54 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { v4 as uuid_v4 } from 'uuid';
-import { Todo } from '../models/Todo';
-import { TodoCrudService } from '../todo-crud.service';
-import { TodoDialogModalComponent } from '../todo-dialog-modal/todo-dialog-modal.component';
 
-export interface DialogData {
-  title: string;
-  desc: string;
-}
+import { TodoDialogModalComponent } from '../todo-dialog-modal/todo-dialog-modal.component';
 
 @Component({
   selector: 'app-todo-dialog',
   templateUrl: './todo-dialog.component.html',
   styleUrls: ['./todo-dialog.component.css'],
-  providers: [TodoCrudService],
 })
-// Dialogin hallinta-puoli
+// Dialogin avauspainike-komponentti
 export class TodoDialogComponent implements OnInit {
-  todo: DialogData;
-
-  constructor(public dialog: MatDialog, private todoService: TodoCrudService) {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(TodoDialogModalComponent, {
+    this.dialog.open(TodoDialogModalComponent, {
       width: '500px',
-    });
-
-    dialogRef.afterClosed().subscribe((todo: DialogData) => {
-      if (todo) {
-        this.todo = todo;
-
-        let newTodo: Todo;
-
-        newTodo = {
-          id: uuid_v4(),
-          title: todo.title,
-          body: todo.desc,
-          completed: false,
-        };
-
-        console.log(newTodo);
-
-        this.todoService.addTodo(newTodo).then(() => {
-          todo.title = ''; // clear input form value
-          todo.desc = ''; // clear input form value
-        });
-      }
     });
   }
 }
